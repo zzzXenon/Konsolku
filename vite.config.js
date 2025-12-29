60b300ab9c5f966a5975fbfa38210be68ac4d253
zzzXenon <royjonathan03082003@gmail.com>
2025-12-29 09:28:31 +0700

    Deployment (vercel) fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+  base: '/', 
+  build: {
+    outDir: 'dist'
+  },
   plugins: [
     tailwindcss(),
     {
